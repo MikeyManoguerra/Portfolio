43062d050455381b76a9e3d453284f0684dc9258
mikeymanoguerra <mikeymanoguerra@gmail.com>
2020-02-16 09:47:10 -0500

    wip: main elements with basic layout generated

---
+++ script/index.js
@@ -0,0 +1,94 @@
+/*global portfolioData util*/
+
+function getBaseElements() {
+  const main = document.getElementById("main")
+  const header = document.getElementById("header")
+  const footer = document.getElementById("footer")
+
+  return { main, header, footer }
+}
+
+// transforms an object of <string>[] and returns an array of html uls
+const handleTechnologyLists = (/*block*/) => Object.keys(portfolioData.technologies).map(techType => util.createSimpleList(portfolioData.technologies[techType], `about__${techType}-`))
+
+function handleCurrentlyLists() {
+  const positionsUl = util.createCurrentlyList(portfolioData.currently.positions, 'about__positions-')
+  const otherUl = util.createCurrentlyList(portfolioData.currently.positions, 'about__other-')
+  
+  return [positionsUl, otherUl]
+}
+
+function nestGridContainer(el, block) {
+  const outer = util.createElementWithClass(el, block)
+  const inner = util.createElementWithClass('div', `${block}__inner`)
+  outer.appendChild(inner)
+
+  return outer
+}
+
+
+function handleAboutSection() {
+  const aboutSection = nestGridContainer("section", 'about')
+  const aboutSectionInner = util.firstChildRef(aboutSection)
+
+  const bio = util.createElementWithClass('p', 'about__bio', portfolioData.bio)
+  aboutSectionInner.appendChild(bio)
+
+  const currently = handleCurrentlyLists()
+  for (list of currently) {
+    aboutSectionInner.appendChild(list)
+  }
+
+  const techLists = handleTechnologyLists()
+  for (list of techLists) {
+    aboutSectionInner.appendChild(list)
+  }
+
+  return aboutSection
+}
+
+
+function handleProject(project) {
+  const projectArticle = nestGridContainer("article", "project")
+  const projectArticleInner = util.firstChildRef(projectArticle)
+
+  const heading = util.createElementWithClass('h2', 'project__heading', project.heading)
+  const copy = util.createElementWithClass('p', 'project__copy', project.description)
+
+  const image = util.createElementWithClass('img', 'project__image')
+  image.src = project.image.src
+  image.alt = project.image.alt
+  
+  const imageWrapper = util.createElementWithClass('div', 'project__image-wrapper')
+  imageWrapper.appendChild(image)
+
+  
+  const git = util.createElementWithClass('a', 'project__link', 'git')
+  git.setAttribute('href', project.links.git)
+  
+  const live = util.createElementWithClass('a', 'project__link', 'live')
+  live.setAttribute('href', project.links.live)
+  
+  const linkWrapper = util.createElementWithClass('div', 'project__link-wrapper')
+  linkWrapper.appendChild(git)
+  linkWrapper.appendChild(live)
+
+  projectArticleInner.appendChild(heading)
+  projectArticleInner.appendChild(copy)
+  projectArticleInner.appendChild(imageWrapper)
+  projectArticleInner.appendChild(linkWrapper)
+
+  return projectArticle
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const { main, header, footer } = getBaseElements()
+  const aboutSection = handleAboutSection()
+  main.appendChild(aboutSection)
+
+  const projects = portfolioData.projects.map(handleProject)
+  for(project of projects){
+    main.appendChild(project)
+  }
+});
---
+++ script/util.js
@@ -0,0 +1,44 @@
+const util = (function () {
+
+  const firstChildRef = el => el.firstElementChild
+
+  function createListItem(text, classPrefix) {
+    return createElementWithClass('li', `${classPrefix}list-item`, text)
+  }
+
+  function createSimpleList(array, classPrefix) {
+    const ul = document.createElement('ul')
+    ul.setAttribute('class', `${classPrefix}list`)
+    array.forEach(item => ul.appendChild(createListItem(item, classPrefix)))
+
+    return ul
+  }
+
+  // not really a util
+  function createCurrentlyList(array, classPrefix) {
+    const ul = createElementWithClass('ul', `${classPrefix}list`)
+    array.forEach(position => {
+      const li = createElementWithClass('li', `${classPrefix}list-item`)
+      li.append(createElementWithClass('p', '', position.location))
+      li.append(createElementWithClass('p', '', position.description))
+      ul.append(li)
+    })
+    return ul
+  }
+
+  function createElementWithClass(type, className='', text='') {
+    const el = document.createElement(type)
+    if(className) el.setAttribute('class', className)
+    if(text) el.innerText = text
+    
+    return el
+  }
+
+  return {
+    firstChildRef,
+    createSimpleList,
+    createCurrentlyList,
+    createElementWithClass,
+  }
+
+})()
